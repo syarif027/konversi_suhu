10055a03de705564a9615e93424e412518b32a0f
syarif027 <syarifhidayat2791@gmail.com>
2023-11-02 14:40:36 +0800

    Initial commit

---
+++ src/main/java/com/mycompany/konversi_suhu/Konversi_suhu.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.konversi_suhu;
+
+/**
+ *
+ * @author User
+ */
+public class Konversi_suhu {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
